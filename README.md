2a4f9cf8eac551c8dba61e2893528f730b48a68b
Suvorov Daniil <severecloud@gmail.com>
2018-12-01 16:56:44 +0300

    Golang + localStorage

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# Генератор клавиатуры для VK
+# [Генератор клавиатуры для VK](https://severecloud.github.io/vk-keyboard)
 
 Генерирует [клавиатуру](https://vk.com/dev/bots_docs_3) для ботов. Проверить клавиатуру можно отправив JSON [боту](https://vk.com/im?sel=-174472256).
-
-![image](https://pp.userapi.com/c844720/v844720651/13ae6a/tqplV5ZBQ7M.jpg)
-
-[severecloud.github.io/vk-keyboard](https://severecloud.github.io/vk-keyboard)
 
 ## Поддерживаеммые языки
 
 - JSON
+- Golang
 
-## Установка
+## Запустить локально
 
 Код основан на магически исчезающем фреймоврке [Svelte](https://svelte.technology/)
 
@@ -19,10 +16,14 @@ cd vk-keyboard
 npm install
 ```
 
+Запустить сервер на [localhost:5000](http://localhost:5000):
+
 ```bash
 npm run dev
 ```
 
-Сервер запустится на [localhost:5000](http://localhost:5000).
+Собрать проект без сервера:
 
-
+```bash
+npm run build
+```
--- src/main.js
+++ src/main.js
@@ -1,52 +1,61 @@
 import App from './App.html';
 
+var raw_keyboard = localStorage.getItem('keyboard');
+
+if (raw_keyboard) {
+  var keyboard = JSON.parse(raw_keyboard);
+} else {
+  var keyboard = {
+    one_time: false,
+    buttons: [
+      [
+        {
+          action: {
+            type: 'text',
+            payload: '{"button": "1"}',
+            label: 'Red'
+          },
+          color: 'negative'
+        },
+        {
+          action: {
+            type: 'text',
+            payload: '{"button": "2"}',
+            label: 'Green'
+          },
+          color: 'positive'
+        }
+      ],
+      [
+        {
+          action: {
+            type: 'text',
+            payload: '{"button": "3"}',
+            label: 'White'
+          },
+          color: 'default'
+        },
+        {
+          action: {
+            type: 'text',
+            payload: '{"button": "4"}',
+            label: 'Blue'
+          },
+          color: 'primary'
+        }
+      ]
+    ]
+  }
+}
+
 const app = new App({
 	target: document.body,
 	data: {
-		keyboard: {
-      one_time: false,
-      buttons: [
-        [
-          {
-            action: {
-              type: 'text',
-              payload: '{"button": "1"}',
-              label: 'Red'
-            },
-            color: 'negative'
-          },
-          {
-            action: {
-              type: 'text',
-              payload: '{"button": "2"}',
-              label: 'Green'
-            },
-            color: 'positive'
-          }
-        ],
-        [
-          {
-            action: {
-              type: 'text',
-              payload: '{"button": "3"}',
-              label: 'White'
-            },
-            color: 'default'
-          },
-          {
-            action: {
-              type: 'text',
-              payload: '{"button": "4"}',
-              label: 'Blue'
-            },
-            color: 'primary'
-          }
-        ]
-      ]
-    },
+		keyboard: keyboard,
     i:0,
     j:0,
-    errorValid: ""
+    errorValid: "",
+    select: "JSON"
 	}
 });
 

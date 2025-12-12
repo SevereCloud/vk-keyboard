4cb13a5f5d7ed32dbfa924f2289562823e547669
SevereCloud <SevereCloud@users.noreply.github.com>
2025-12-12 20:57:23 +0000

    deploy: 984e5ec11ea8a3b0117be106808b3500e10e95ee

---
+++ _next/static/0rOmuiET1W6PTxj-NnKaC/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/vk-keyboard/_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
